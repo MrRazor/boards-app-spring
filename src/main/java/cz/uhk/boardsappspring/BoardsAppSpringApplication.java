cdd2dd60f7f5ffe946146da7a8661a81184e90c2
MrRazor <jantomasstekl@gmail.com>
2023-12-29 12:55:12 +0100

    initial commit

---
+++ src/main/java/cz/uhk/boardsappspring/BoardsAppSpringApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.boardsappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardsAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardsAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/uhk/boardsappspring/BoardsAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.boardsappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardsAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
